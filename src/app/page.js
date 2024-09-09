d5003bf3f1874679e69d337a6368607b1d9303e7
Megan Palmquist <megpalmq@iu.edu>
2024-09-09 12:55:45 -0400

    fixed the layout of the products and added images

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,36 @@
 "use client";
 import homeStyles from "@/app/page.module.css";
 
+
 export default function Home() {
   return (
-    <main>
+    <>
       <nav className={homeStyles.homeNav}>
         <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Products</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#products">Products</a></li>
+        <li><a href="#about">About</a></li>
         </ul>
       </nav>
-      <div>
-        
-      </div>
-    </main>
+      
+      <main className={homeStyles.main}>
+        <h1 className={homeStyles.title}>Welcome to the homepage!</h1>
+        <div className={homeStyles.products}>
+        <section className={homeStyles.productShowcase}>
+        <div className={homeStyles.product1}>
+          <img src="/product1.jpg" alt="Product 1" className={homeStyles.productImage1} />
+            <h2 style={{color:'white'}}>Product 1</h2>
+            <p style={{color:'white'}}>$19.99</p>
+          </div>
+
+          <div className={homeStyles.product2}>
+          <img src="/product2.jpg" alt="Product 2" className={homeStyles.productImage2} />
+          <h2 style={{ color: 'white', margin: '0.5rem 0' }}>Product 1</h2>
+            <p style={{color: 'white',}}>$6.99</p>
+          </div>
+        </section>
+        </div>
+      </main>
+    </>
   );
 }
